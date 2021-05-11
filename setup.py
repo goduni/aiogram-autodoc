5103d16df1e0e12653f745f1ce50500bb51dd3f2
Bunk100 <xxlordbunk150@gmail.com>
2021-05-12 01:48:47 +0300

    changes at setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiogram_autodoc',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/viuipan/aiogram-autodoc',
     author_email='viuipan@gmail.com',
     license='MIT',
     description='Generates command documentation from handlers',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     packages=['aiogram_autodoc'],
     setup_requires=['aiogram<3']
 )
