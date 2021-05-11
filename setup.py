d2e6445bd3c62c7c1b4d85b3862d137ffcfbc38a
Bunk100 <xxlordbunk150@gmail.com>
2021-05-12 01:40:09 +0300

    update author email at setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='aiogram_autodoc',
     version='0.0.1',
     url='https://github.com/viuipan/aiogram_autodoc',
+    author_email='viuipan@gmail.com',
     license='MIT',
     description='Generates command documentation from handlers',
     packages=['aiogram_autodoc'],
